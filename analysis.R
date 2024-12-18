a6e0fead28ed9040906551b07b6a15146ad1413e
Philip L. Wolper <phi.wolper@tum.de>
2024-12-18 17:30:48 +0100

    figures and data

--- analysis.R
+++ analysis.R
@@ -1,14 +1,13 @@
 library(ggplot2)
 library(stringr)
 
-N <- 5000
+N <- 50000
 teInitialCount <- 1
 teJumpP <- 0.01
 teDeathRate <- 0.0005
 simTime <- 2000
 
-## File name parsing function
-
+# File name parsing function
 get_simfiles <- function(path='output/csv', N, teInitialCount, teJumpP, teDeathRate, simTime) {
   pattern <- paste0('haploid_', 'N', N, '_teInitialCount', teInitialCount, '_teJumpP', teJumpP, '_teDeathRate',
                     format(teDeathRate, scientific=FALSE), '_simTime', simTime)
@@ -16,11 +15,8 @@ get_simfiles <- function(path='output/csv', N, teInitialCount, teJumpP, teDeathR
   return(files)
 }
 
-files <- get_simfiles('output/csv', N, teInitialCount, teJumpP, teDeathRate, simTime)
-files # vector of files
-
+# Extract values using regular expressions
 extract_params <- function(file) {
-  # Extract values using regular expressions
   N <- as.numeric(str_extract(file, "(?<=_N)\\d+"))
   teJumpP <- as.numeric(str_extract(file, "(?<=_teJumpP)\\d+\\.\\d+"))
   teDeathRate <- as.numeric(str_extract(file, "(?<=_teDeathRate)\\d+\\.\\d+"))
@@ -30,6 +26,7 @@ extract_params <- function(file) {
   return(params)
 }
 
+# Extract TE mean to dataframe
 extract_TE_data <- function(file) {
   df <- read.csv(file, stringsAsFactors = FALSE)
 
@@ -43,20 +40,33 @@ extract_TE_data <- function(file) {
   return(output_data)
 }
 
+# Inifitite population equilibrium
+expected_equilibrium <- function(teJumpP, teDeathRate) {
+  (1+(teJumpP/teDeathRate))
+}
+
+
+files <- get_simfiles('output/csv', N, teInitialCount, teJumpP, teDeathRate, simTime)
+paste("Simulation files:", length(files)) # vector of files
 
 df <- do.call(rbind, lapply(files, extract_TE_data))
 str(df)
 
-ggplot(df, aes(x = generation, y = means, group = replicate)) +
-  geom_line(size = 0.5, color ='blue') +  # Line plot for trajectories
+title <- paste(' N=', N, ' | ', 'teJumpP=', teJumpP, ' | ', 'teDeathRate:', teDeathRate)
+
+ggplot(df, aes(x = generation, y = means, color = factor(replicate), group = replicate)) +
+  geom_line(size = 0.5, alpha=0.4) +  # Line plot for trajectories
   labs(
-    title = "Simulation Trajectories: Means vs Generation",
+    title = paste('Parameters: ', title),
     x = "Generation",
     y = "Mean Value",
     color = "Replicate"
   ) +
-  theme_minimal() +
-  theme(
-    plot.title = element_text(hjust = 0.5),  # Center the plot title
-    legend.position = "none"               # Place legend on the right
-  )
+  #ylim(0, 100) +
+  geom_hline(yintercept = expected_equilibrium(teJumpP, teDeathRate), linetype="dashed", color='red') +
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5),  # Center the plot title
+        legend.position = "none")               # Place legend on the right)
+
+filename <- paste0('haploid_N', N, 'teJumpP', teJumpP, '_teDeathRate', teDeathRate, '_simTime', simTime, '.png')
+ggsave(filename = paste0('output/fig/', filename), height=7, width=10)
