8b7af5c33096160867c4be3809d0810bb1592ac1
Philip Wolper <philip.wolper@gmail.com>
2024-12-18 12:18:37 +0100

    plotting

--- analysis.R
+++ analysis.R
@@ -1,29 +1,62 @@
-N <- 10
+library(ggplot2)
+library(stringr)
+
+N <- 5000
 teInitialCount <- 1
 teJumpP <- 0.01
-teDeathRate <- format(0.0005, scientific = FALSE)
-simTime <- 100
+teDeathRate <- 0.0005
+simTime <- 1000
 
 ## File name parsing function
-path <- file.path('output/csv')   # portable
 
-pattern <- paste0('haploid_', 'N', N, '_teInitialCount', teInitialCount,
-                  '_teJumpP', teJumpP, '_teDeathRate', teDeathRate, '_simTime', simTime)
-print(pattern)
+get_simfiles <- function(path='output/csv', N, teInitialCount, teJumpP, teDeathRate, simTime) {
+  pattern <- paste0('haploid_', 'N', N, '_teInitialCount', teInitialCount, '_teJumpP', teJumpP, '_teDeathRate',
+                    format(teDeathRate, scientific=FALSE), '_simTime', simTime)
+  files <- list.files(path, pattern = pattern, full.names=TRUE)
+  return(files)
+}
 
-files <- list.files(path, pattern = pattern, full.names=TRUE)
-##print(str(files[1]))
+files <- get_simfiles('output/csv', N, teInitialCount, teJumpP, teDeathRate, simTime)
+files # vector of files
+
+extract_params <- function(file) {
+  # Extract values using regular expressions
+  N <- as.numeric(str_extract(file, "(?<=_N)\\d+"))
+  teJumpP <- as.numeric(str_extract(file, "(?<=_teJumpP)\\d+\\.\\d+"))
+  teDeathRate <- as.numeric(str_extract(file, "(?<=_teDeathRate)\\d+\\.\\d+"))
+  simTime <- as.numeric(str_extract(file, "(?<=_simTime)\\d+"))
+
+  params <- list(N = N, teJumpP = teJumpP, teDeathRate = teDeathRate, simTime = simTime)
+  return(params)
+}
+
+extract_TE_data <- function(file) {
+  df <- read.csv(file, stringsAsFactors = FALSE)
+
+  params <- extract_params(file)
+  means <- rowMeans(df)
+  generation <- seq.int(nrow(df))
+  replicate <- rep(file, times = length(generation)) # adjust generation time and output in slim
+
+  output_data <- data.frame(replicate, generation, params$N, params$teJumpP, params$teDeathRate, params$simTime, means)
+  names(output_data)[3:6] <- c("N", "teJumpP", "teDeathRate", "simTime")
+  return(output_data)
+}
 
 
-i <- 1
+df <- do.call(rbind, lapply(files, extract_TE_data))
+str(df)
 
-file <-  files[i]
-
-df <- read.csv(file, stringsAsFactors = FALSE)
-df
-
-meanTE <- rowMeans(df)
-generation <- seq.int(nrow(df))
-rep <- rep(i, times = length(generation)) # adjust generation time and output in slim
-means <- data.frame(rep, generation, meanTE)
-means
+ggplot(df, aes(x = generation, y = means, group = replicate)) +
+  geom_line(size = 0.8, color ='blue') +  # Line plot for trajectories
+  labs(
+    title = "Simulation Trajectories: Means vs Generation",
+    x = "Generation",
+    y = "Mean Value",
+    color = "Replicate"
+  ) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5),  # Center the plot title
+    legend.position = "none"               # Place legend on the right
+  )
